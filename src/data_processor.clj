163d36f8b8687cec22291a7b88c46b38b2d9dcb6
Nahuel Galvan <Nahue@Nahuels-MacBook-Pro.local>
2018-04-02 20:32:21 -0300

    processor-data

--- src/data_processor.clj
+++ src/data_processor.clj
@@ -21,14 +21,14 @@
 
   ;rule
   (def conditions (nth (nth rule 1) 1))
-  (def parameters (nth (nth rule 1) 0))
-  (prn parameters)
+  ; (def parameters (nth (nth rule 1) 0))
+  ; (def name (nth rule 0))
+  ; (def counter-to-update {:key :0})
 
     ;en funcion a los parametros se a cual contador aumentar.
-    (if (empty? parameters) "vacio" "lleno")
+    ;(if (empty? parameters) "vacio" "lleno")
 
-    ;evaluar condicion. Si la cumple devuelve true
-    (evaluateConditions data conditions)
+      (evaluateConditions data conditions)
 
     )
 
@@ -38,12 +38,12 @@
 
 (defn process-data [state new-data]
   ;aplico cada rule al dato a procesar.
-  ;devuelvo un hashmap como el de counters para luego sumar los contadores en true. ej {:email-count :true , :spam-count :false}
+  ;devolverá un hashmap como el de counters para luego sumar los contadores en true. ej {:email-count :true , :spam-count :false}
     (for [rule (nth state 1)]
        (applyRule new-data rule)
-
+       
      )
- 
+
 
     )
 (defn query-counter [state counter-name counter-args]
