fe83cb5e5b45ebe4e85f2b37af411eea3d497247
Gnahue <nahuelgalvan@gmail.com>
2018-04-07 19:47:38 -0300

    data-processor-test

---
+++ test/process_data_test.clj
@@ -0,0 +1,25 @@
+ (ns process-data-test
+   (:require [clojure.test :refer :all]
+     [data-processor :refer :all]))
+
+     (def rules '((define-counter "email-count" []
+                    true)
+                  (define-counter "spam-count" []
+                    (current "spam"))
+                  (define-signal {"spam-fraction" (/ (counter-value "spam-count" [])
+                                                     (counter-value "email-count" []))}
+                    true)
+                  ; (define-counter "spam-important-table" [(current "spam")
+                  ;                                         (current "important")]
+                  ;   true)))
+                  ))
+    (def counter-state (initialize-counters rules))
+    (def signals (save-signal-rules rules))
+    (def rules (save-counter-rules rules))
+
+    (def email-count-rule [:email-count (get rules :email-count )])
+
+ (deftest name-and-signal-evaluation-test
+
+    (testing "increment counter"
+    (is (= (inc-counter email-count-rule counter-state) {:email-count 1, :spam-count 0}))))
