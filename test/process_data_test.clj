46f28d7e601ca2411b1f6c4de9c3872aaf2142cb
gnahue <nahuelgalvan@gmail.com>
2018-04-10 16:59:50 -0300

    tests inc-counter

--- test/process_data_test.clj
+++ test/process_data_test.clj
@@ -26,12 +26,29 @@
   (def counter-state (initialize-counters rules))
 
   (def data {"spam" true})
+  (def data-parameteres {"spam" true "important" true})
 
   (def email-count-rule ["email-count" (get counters-rules "email-count" )])
+  (def spam-important-count-rule ["spam-important-table" (get counters-rules "spam-important-table" )])
+
   (deftest increment-counter-test-test
      (testing "increment counter"
      (is (= (inc-counter initial-state email-count-rule data counter-state) {"email-count" 1, "spam-count" 0, "spam-important-table" {}}))
      ))
+
+  (deftest increment-counter-test-test
+    (testing "increment counter with parameters"
+    (is (= (inc-counter initial-state spam-important-count-rule  data-parameteres counter-state) {"email-count" 0, "spam-count" 0, "spam-important-table" {[true true] 1}})))
+    )
+
+    (deftest increment-counter-test-test
+      (testing "increment counter mixed values"
+      (def counter-0 (inc-counter initial-state spam-important-count-rule  data-parameteres counter-state))
+      (def data-parameteres-0 {"important" true "spam" true})
+      (def data-parameteres-1 {"important" false "spam" true})
+      (is (= (inc-counter initial-state spam-important-count-rule  data-parameteres-0 counter-0) {"email-count" 0, "spam-count" 0, "spam-important-table" {[true true] 2}}))
+        (is (= (inc-counter initial-state spam-important-count-rule  data-parameteres-1 counter-0) {"email-count" 0, "spam-count" 0, "spam-important-table" {[true true] 1 [true false] 1}})))
+      )
 
   (deftest evaluate-condition-true-from-rule
     (testing "evaluate-conditions-from-rule should return true if the condition is fulfilled or false if not."
