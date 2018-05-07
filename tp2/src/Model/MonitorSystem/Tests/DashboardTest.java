76991e8f616859f014581ce7e7ff89e1e2327556
mariaflorenciaprado <pradomflorencia@gmail.com>
2018-05-07 04:33:34 -0300

    Dashboard:AddQuery Test

--- tp2/src/Model/MonitorSystem/Tests/DashboardTest.java
+++ tp2/src/Model/MonitorSystem/Tests/DashboardTest.java
@@ -2,6 +2,10 @@ package tp2.src.Model.MonitorSystem.Tests;
 
 import junit.framework.TestCase;
 import tp2.src.Model.MonitorSystem.Dashboard;
+import tp2.src.Model.MonitorSystem.Query;
+import tp2.src.Model.MonitorSystem.Rule;
+
+import java.time.Duration;
 
 public class DashboardTest extends TestCase {
     private Dashboard dummyDashboard;
@@ -12,10 +16,13 @@ public class DashboardTest extends TestCase {
 
     public void testEquals() {
         assertTrue(dummyDashboard.equals("d1"));
-
     }
 
     public void testAddQuery() {
-
+        Rule rule = new Rule();
+        Duration duration =  Duration.ofHours(1);
+        Query query = new Query("countTickets", duration, rule );
+        this.dummyDashboard.addQuery(query);
+        assertEquals(1, this.dummyDashboard.getQueries().size());
     }
 }
