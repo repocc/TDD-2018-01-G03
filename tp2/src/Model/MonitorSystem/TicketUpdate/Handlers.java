0ee4b6e25ff176b175dbb27ae975497eaa2740fc
mariaflorenciaprado <pradomflorencia@gmail.com>
2018-06-02 03:00:21 -0300

    GET TICKET STATE FROM REQUEST OK

--- tp2/src/Model/MonitorSystem/TicketUpdate/Handlers.java
+++ tp2/src/Model/MonitorSystem/TicketUpdate/Handlers.java
@@ -18,26 +18,11 @@ public class Handlers {
 
         @Override
         public void handle(HttpExchange he) throws IOException {
-
-
-            byte[] buf = new byte[2040];
-            System.out.println("Server: " + he.getRequestURI());
-            try (InputStream is = he.getRequestBody()) {
-                while (is.read(buf) != -1) ;
-
-                String v = new String( buf, Charset.forName("UTF-8") );
-            System.out.println();
-
-//                String ticket = he.getRequestHeaders().get("state").get(0);
-//
-//                System.out.println("Ticket recibido: " + ticket);
-
             String response = "<h1>Server start success if you see this message</h1>" + "<h1>Port: " + 9000 + "</h1>";
             he.sendResponseHeaders(200, response.length());
             OutputStream os = he.getResponseBody();
             os.write(response.getBytes());
             os.close();
-        }
     }}
 
 
@@ -50,21 +35,33 @@ public class Handlers {
             G3Translate = new TicketSystemG3Translator();
         }
 
+        public String getBody(HttpExchange he){
+            String ticket = "";
+            byte[] buf = new byte[200];
+            try (InputStream is = he.getRequestBody()) {
+                while (is.read(buf) != -1) ;
+                ticket = new String( buf, Charset.forName("UTF-8") );
+            } catch (IOException e) {
+                System.out.println("Can not read RequestBody");
+                e.printStackTrace();
+            }
+            int iend = ticket.indexOf("=");
+            ticket = ticket.substring(0, iend);
+            System.out.println("Ticket arrived: " + ticket + "...");
+            return ticket;
+        }
+
         @Override
         public void handle(HttpExchange he) throws IOException {
-            String ticket = he.getRequestHeaders().get("State").get(0);
-
-            System.out.println("Ticket recibido: " + ticket);
-
+            String ticket = getBody(he);
             JSONObject ticketJson = new JSONObject();
             ticketJson.put("state", ticket);
             try {
-                System.out.println("Enviado al Dealear");
+                System.out.println("Ticket sent to Dealear...");
                 ticketsDealer.updateTicket(G3Translate.translateTicket(ticketJson));
             } catch (RuleNotFoundException e) {
                 e.printStackTrace();
             }
-
 
             String response = "TICKET UPDATED: " + ticket;
             he.sendResponseHeaders(201, response.length());
